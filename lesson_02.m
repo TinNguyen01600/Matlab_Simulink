750508d2b931dcb859973d121df53f4cc2c9cc97
TinNguyen <ductin16111@gmail.com>
2022-11-04 12:28:01 +0200

    arrays & matrices pt 2

--- lesson_02.m
+++ lesson_02.m
@@ -1,3 +1,4 @@
+
 clear; close all; clc;
 % ctrl + R: comment multi lines
 a = ones(4,3);
@@ -8,5 +9,20 @@ r = rand(5,4);  % create randoms, NOT repeat each time
 %%
 % 2 different sections of code running respectively
 clear; close all; clc;
-rq = 45
-c = 'this is cool'
+rq = 45;
+c = 'this is cool';
+%% Array operations
+clear; clc; 
+% a = 2;
+% b = 3;
+% c = a + b;
+% d = a - b;
+% e = a * b;
+% f = a / b;
+% m = mod(2,3);    % remainder
+% p = a ^ b;
+
+a = rand(1,5)
+b = rand(1,5)
+c = a + b
+d = a - b
