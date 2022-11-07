a2057a310e3355b6bf2942ef2f8d066ea41d4bb9
TinNguyen <ductin16111@gmail.com>
2022-11-07 12:16:49 +0200

    matlab struct

---
+++ lesson_04.m
@@ -0,0 +1,16 @@
+clear; close all; clc;
+% car = struct;
+car(1).brand = 'Honda';
+car(1).make = 'Civic';
+car(1).year = 2017;
+car(1).price = 20000;
+
+car(2).brand = 'Nissa';
+car(2).make = 'Altima';
+car(2).year = 2015;
+car(2).price = 15000;
+
+car(3).brand = 'Honda';
+car(3).make = 'Accord';
+car(3).year = 2019;
+car(3).price = 35000;
