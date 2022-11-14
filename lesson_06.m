11fce4002204da94f52be0e22526e23b98cfbb4f
TinNguyen <ductin16111@gmail.com>
2022-11-14 13:13:23 +0200

    while loop

--- lesson_06.m
+++ lesson_06.m
@@ -10,3 +10,15 @@ for cnt = 1:10
     end
     fprintf("this is end\n")
 end
+%%
+clear; close all; clc;
+a = 0; cnt = 0;
+while cnt <= 100
+    a = a + rand(1);
+    if a >= 25
+        break
+    else
+        cnt = cnt + 1;
+    end
+end
+fprintf('step %3d \t a = %6.4f\n', cnt, a)   % 6 digits before and 4 digits after decimal point
